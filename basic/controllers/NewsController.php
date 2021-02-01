922e969521bbd2f2fca5f4da01199487df5f4cf1
weiya.hsu <weiya.hsu@staff.nueip.com>
2021-02-01 18:17:52 +0800

    tmp

--- basic/controllers/NewsController.php
+++ basic/controllers/NewsController.php
@@ -3,125 +3,45 @@
 namespace app\controllers;
 
 use Yii;
-use app\models\News;
-use app\models\NewsSearch;
-use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
+use yii\rest\ActiveController;
+use \yii\db\Query;
 
-/**
- * NewsController implements the CRUD actions for News model.
- */
-class NewsController extends Controller
+class NewsController extends ActiveController
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function behaviors()
+    public $modelClass = 'app\models\News';
+
+    public function actions()
     {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
-        ];
+        $actions = parent::actions();
+
+        // 使用 "prepareDataProvider()" 方法自定义数据 provider 
+        $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+
+        return $actions;
     }
 
-    /**
-     * Lists all News models.
-     * @return mixed
-     */
-    public function actionIndex()
+    public function prepareDataProvider()
     {
-        $searchModel = new NewsSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        // JOIN
+        // $query = new Query();
+        // $rows = $query->select(['news.id', 'news.title', 'post.id AS post'  , 'post.body'])
+        //     ->from('news')
+        //     ->join('LEFT JOIN', 'post', 'post.newsid = news.id')
+        //     ->all();
+       
+        // 子查詢 count
+        // $rows = Yii::$app->db->createCommand('SELECT * ,(SELECT COUNT(*) FROM `post`) AS `count` FROM news ')
+        //         ->queryAll();
 
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
+        // $query = new Query();
+        // $subQuery = (new Query())->select('COUNT(*)')->from('post');
+        // $rows = $query->select(['id', 'title','count' => $subQuery])
+        // ->from('news')
+        // ->all();
 
-    /**
-     * Displays a single News model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new News model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new News();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing News model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing News model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the News model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return News the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = News::findOne($id)) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException('The requested page does not exist.');
+        // 條件
+        $rows = Yii::$app->db->createCommand('SELECT * FROM news WHERE id == (SELECT newsid FROM post)')
+                ->queryAll();
+        return $rows;
     }
 }
